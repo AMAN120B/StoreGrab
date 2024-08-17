433805ba899a17468bc09cb26c8fdfe870701ee1
AMAN120B <aman8mar@gmail.com>
2024-08-17 14:31:57 +0530

    storeservice back

---
+++ latest/src/app/services/Auth.service.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from './AuthService';
+
+// import { AuthService } from './AuthService.service';
+
+describe('AuthenticationService', () => {
+  let service: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AuthService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
